1f278696f747a4b0e8b58ba4b5721045e8115bad
iamdgao <42900771+iamdgao@users.noreply.github.com>
2018-09-16 10:22:59 +1000

    Add files via upload

---
+++ largerthannum.py
@@ -0,0 +1,16 @@
+def params(a, b):
+    myList = sorted(b)
+    c = int(myList[0])
+    if c > a:
+        return (True) 
+    else:
+        return (False)
+        
+    
+ans = params(1, [2,3,4,5])
+print(ans)
+        
+
+            
+                
+    
